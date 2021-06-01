b168c794db3bdd4a31e1392366d5831b02643ed2
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-01 10:06:43 -0400

    Começando no React-native

---
+++ MyLink/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App(){
+    return(
+      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+
+        <Text style={{fontSize: 25, fontWeight: 'bold'}}>Wesley</Text>
+        <Text>Segundo texto</Text>
+
+      </View>
+    )
+}
+
+
+
+
